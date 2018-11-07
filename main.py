46fc8c3bbf70f2013a4634d4bfe140b7b5a3a757
DamourYouKnow <damourba@gmail.com>
2018-11-07 13:27:09 -0500

    added pygame loop and updated requirements.txt

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import sys
+import pygame
+
 if __name__ == '__main__':
     print("MATH 3808 Final Project")
+
+    pygame.init()
+    screen = pygame.display.set_mode((800, 500))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
