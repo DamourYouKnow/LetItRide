459bc7dcea45fb70cfeea8a5ea1108b739aad5de
damouryouknow <damourba@gmail.com>
2018-12-14 14:30:45 -0500

    added docstrings to core logic

--- core.py
+++ core.py
@@ -18,7 +18,6 @@ class Suit(Enum):
             Suit.clubs: "C",
             Suit.hearts: "H",
             Suit.spades: "S"
-
         }
         return strings[self]
 
@@ -64,7 +63,6 @@ class HandType(Enum):
             HandType.pair: "Nothing",
             HandType.high: "Nothing",
 			
-			
 			#sidebet strings 
             HandType.mini_royal: "Mini Royal",
             HandType.three_of_kind_side: "Three of a Kind",
@@ -73,31 +71,48 @@ class HandType(Enum):
             HandType.straight_side: "Straight",
             HandType.pair_side: "Pair",
             HandType.high_side: "Nothing"
-           
-            
-			
         }
         return strings[self]
-
 
 class Card:
     """
     Class representing a playing card.
     """
     def __init__(self, rank: int, Suit: Suit):
+        """
+        Create an instance of a card.
+        
+        Arguments:
+            rank {int} -- Rank of card (A=1,J=11,Q=12,K=13)
+            Suit {Suit} -- Suit of card.
+        """
         self._rank = rank
         self._Suit = Suit
 
     @property
-    def rank(self):
+    def rank(self) -> int:
+        """
+        Returns:
+            int -- Rank of card.
+        """
         return self._rank
 
     @property
-    def Suit(self):
+    def Suit(self) -> Suit:
+        """ 
+        Returns:
+            Suit -- Suit of card.
+        """
+
         return self._Suit
 
     @property
-    def filename(self):
+    def filename(self) -> str:
+        """
+        Returns:
+            [str] -- Filepath of card image.
+        """
+
         return "./assets/" + str(self.rank) + str(self.Suit) + ".png"
 
     def __eq__(self, other: 'Card') -> bool:
@@ -154,6 +169,11 @@ class Hand:
 
     @property
     def cards(self) -> List[Card]:
+        """
+        Returns:
+            List[Card] -- Cards in hand.
+        """
+
         return self._cards
 
     def __len__(self):
@@ -164,59 +184,28 @@ class Hand:
             yield card
 
     def payout(self, bet: int) -> int:
+        """
+        Computes the payout of a hand for a specified bet.
+        
+        Arguments:
+            bet {int} -- Bet amount.
+        
+        Returns:
+            int -- Payout of bet.
+        """
         if self.type in Hand.payouts:
             # +1 for returning initial bet
             return (Hand.payouts[self.type]+1) * bet
         return 0
 
     @property
-    def type_side(self) -> HandType:
-        hand = self._cards.copy()
-        del hand[4]
-        del hand[3]
-        hand.sort()
-        size=3
-        #size = len(hand)
-
-        royals = [11, 12, 13]
-        ranks = [card.rank for card in hand]
-        is_royal = all([r in ranks for r in royals])
-        is_straight = ranks == list(range(ranks[0], ranks[-1] + 1)) or is_royal
-        is_flush = len([c for c in hand if c.Suit == hand[0].Suit]) == size
-
-        if is_royal and is_flush:
-            return HandType.mini_royal
-        if is_straight and is_flush:
-            return HandType.straight_flush
-        if is_straight:
-            return HandType.straight
-        if is_flush:
-            return HandType.flush
-
-        count_map = {
-            c.rank: len([c2 for c2 in hand if c2.rank == c.rank]) for c in hand
-        }
-        counts = list(count_map.values())
-
-        #if counts.count(4) == 1:
-         #   return HandType.four_of_kind
-        #if counts.count(3) == 1 and counts.count(2) == 1:
-          #  return HandType.full_house
-        if counts.count(3) == 1:
-            return HandType.three_of_kind
-        #if counts.count(2) == 2:
-         #   return HandType.two_pair
-        if counts.count(2) == 1:
-            #if [c for c in hand if c.rank in royals and count_map[c.rank] == 2]:
-            return HandType.pair_side
-            #else:
-             #   return HandType.pair
-
-        return HandType.high_side
-        
-    
-    @property
     def type(self) -> HandType:
+        """
+        Determines the type of a hand (Two pair, straight, flush, ...).
+        
+        Returns:
+            HandType -- Type of hand.
+        """
         hand = self._cards.copy()
         hand.sort()
         size = len(hand)
@@ -257,22 +246,84 @@ class Hand:
 
         return HandType.high
 
+    @property
+    def type_side(self) -> HandType:
+        """
+        Determines the type of a side bet hand.
+
+        Returns:
+            HandType -- Hand type.
+        """
+        hand = self._cards.copy()
+        del hand[4]
+        del hand[3]
+        hand.sort()
+        size=3
+
+        royals = [11, 12, 13]
+        ranks = [card.rank for card in hand]
+        is_royal = all([r in ranks for r in royals])
+        is_straight = ranks == list(range(ranks[0], ranks[-1] + 1)) or is_royal
+        is_flush = len([c for c in hand if c.Suit == hand[0].Suit]) == size
+
+        if is_royal and is_flush:
+            return HandType.mini_royal
+        if is_straight and is_flush:
+            return HandType.straight_flush
+        if is_straight:
+            return HandType.straight
+        if is_flush:
+            return HandType.flush
+
+        count_map = {
+            c.rank: len([c2 for c2 in hand if c2.rank == c.rank]) for c in hand
+        }
+        counts = list(count_map.values())
+
+        if counts.count(3) == 1:
+            return HandType.three_of_kind
+  
+        if counts.count(2) == 1:
+            return HandType.pair_side
+
+        return HandType.high_side
+        
 
 class Deck:
     """
     Class representing a deck of cards.
     """
     def __init__(self, count: int=1):
+        """
+        Create a deck of cards with a specified deck size.
+        
+        Keyword Arguments:
+            count {int} -- Size of deck (default: {1})
+        """
+
         self._cards = Deck._create_deck(count)
 
     @property
     def cards(self) -> List[Card]:
+        """
+        Returns:
+            List[Card] -- List of cards in deck.
+        """
         return self._cards
 
     def draw(self) -> Card:
+        """
+        Draws a card from the top of a deck.
+        
+        Returns:
+            Card -- Drawn card.
+        """
         return self._cards.pop()
 
     def shuffle(self):
+        """
+        Shuffles the deck.
+        """
         random.shuffle(self._cards)
 
     def __len__(self):
@@ -290,9 +341,17 @@ class Deck:
 
 class Game:
     """
-    Class representing a blackjack game.
+    Class representing a Let it Ride game.
     """
     def __init__(self, decks: int = 1, name: str="Player", money: int = 1000):
+        """
+        Creates an instance of a game.
+        
+        Keyword Arguments:
+            decks {int} -- Number of card decks to use. (default: {1})
+            name {str} -- Name of player (default: {"Player"})
+            money {int} -- Starting bankroll of player. (default: {1000})
+        """
         self._deck_count = decks
         self._deck = Deck(decks)
         self._player = Player(self, name, money)
@@ -300,16 +359,28 @@ class Game:
 
     @property
     def deck(self) -> Deck:
+        """
+        Returns:
+            Deck -- Game deck.
+        """
         return self._deck
     
     @property
     def player(self) -> 'Player':
+        """
+        Returns:
+            [Player] -- Game player.
+        """
         return self._player
     
     def deal(self):
+        """
+        Deal 5 cards into the player's hand.
+        """
         self._deck = Deck(self._deck_count) # We may want to change this logic.
         self._deck.shuffle()
         self.player.hand = Hand([self._deck.draw() for _ in range(5)])
+
 
 class Settings:
     def __init__(
@@ -344,11 +415,20 @@ class Settings:
     def card(self):
         return self._card
     
+
 class Player:
     """
     Class representing a player.
     """
     def __init__(self, game: Game, name: str, money: int):
+        """
+        Creates and instance of a player.
+        
+        Arguments:
+            game {Game} -- Game player is participating in.
+            name {str} -- Name of player.
+            money {int} -- Starting bankroll of player.
+        """
         self._game = game
         self._name = name
         self._money = money
@@ -359,49 +439,104 @@ class Player:
 
     @property
     def name(self) -> str:
+        """
+        Returns:
+            str -- Name of player.
+        """
         return self._name
 
     @property 
     def money(self) -> int:
+        """
+        Returns:
+            int -- Bankroll of player.
+        """
         return self._money
 
     @money.setter
     def money(self, value: int):
+        """
+        Arguments:
+            value {int} -- Bankroll of player.
+        """
+
         self._money = value
 
     @property
     def full_bet(self) -> int:
+        """
+        Returns:
+            int -- Combined bet of player.
+        """
         return self._full_bet
 
     @property
     def portion_bet(self) -> int:
+        """
+        Returns:
+            int -- Individual unit bet of player.
+        """
         return self._portion_bet
 
     @property
     def hand(self) -> Hand:
+        """
+        Returns:
+            Hand -- Hand of player.
+        """
         return self._hand
 
     @hand.setter
     def hand(self, value: Hand):
+        """
+        Arguments:
+            value {Hand} -- Hand of player.
+        """
         self._hand = value
 
     def draw(self, count: int=1):
+        """
+        Draw a specified number of cards from the game deck into this player's 
+        hand.
+
+        Keyword Arguments:
+            count {int} -- Number of cards to draw. (default: {1})
+        """
         for _ in range(0, count):
             self._hand.cards.append(self._game.deck.draw())
 
     def bet(self, bet: int):
+        """
+        Places a bet for a player.
+
+        Remarks:
+            The full bet is 3 times this amount.
+        
+        Arguments:
+            bet {int} -- Indivivual bet amount.
+        """
         self._money -= bet * 3
         self._full_bet = bet * 3
         self._portion_bet = bet
 
     def pull(self):
+        """
+        Pull a portion of a player's bet.
+        
+        Raises:
+            Exception -- Raised if the last bet is pulled.
+        """
         if self._full_bet <= self._portion_bet:
             raise Exception("Cannot full 1st bet")
         self._full_bet -= self._portion_bet
         self._money += self._portion_bet
 
     def payout(self):
+        """
+        Adds the payout of a player's hand to their bankroll.
+        """
         self._money += self._hand.payout(self._full_bet)
+
 
 class Statistics:
     @staticmethod
